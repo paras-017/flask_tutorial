1870b6479e30fd840aeb3de313a3c088c416ebca
paras-017 <paraschauhan0017@gmail.com>
2023-06-22 17:55:16 +0530

    SQLAlchemy setup completed

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.app_context().push()
+db = SQLAlchemy(app)
+
+
+
+# Schema
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    description = db.Column(db.String(500), nullable = False)
+    date_created = db.Column(db.DateTime,default = datetime.utcnow)
+
+    def __repr__(self) -> str:      #The  purpose of __repr__ is to provide a helpful and unambiguous representation of an object for debugging purposes
+        return f"{self.sno} - {self.title}"
+db.create_all()
 
 @app.route('/')
 def hello():
